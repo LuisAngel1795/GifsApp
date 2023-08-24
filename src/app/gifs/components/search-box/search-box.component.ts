058a7effda45ab7467c3691848cce1de1c3a5000
Luis Silva <luis.guerreros@elektra.com.mx>
2023-08-23 19:07:47 -0600

    httpClient

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-searh-box',
@@ -7,10 +8,20 @@ import { Component } from '@angular/core';
   <input type="text"
   class = "form-control"
   placeholder="Buscar gifs..."
+  (keyup.enter)="searchTag()"
+  #txtTagInput
   >
   `
 })
 
 export class SearchBoxComponent{
-  constructor() { }
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+  constructor(private gifsService: GifsService) { }
+
+  searchTag(){
+    const newTag = this.tagInput.nativeElement.value;
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value='';
+  }
 }
